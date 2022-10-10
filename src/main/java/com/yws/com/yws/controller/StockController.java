90c0be769ef12bfcee8a8e73fe5d819b76052cf7
yws <yws528819@163.com>
2022-10-10 19:06:41 +0800

    增加库存接口

--- src/main/java/com/yws/com/yws/controller/StockController.java
+++ src/main/java/com/yws/com/yws/controller/StockController.java
@@ -2,6 +2,7 @@ package com.yws.com.yws.controller;
 
 import com.yws.com.yws.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +10,10 @@ public class StockController {
 
     @Autowired
     private StockService stockService;
+
+    @GetMapping("/stock")
+    public String deduct() {
+        stockService.deduct();
+        return "hello stock deduct";
+    }
 }
--- src/main/java/com/yws/com/yws/service/StockService.java
+++ src/main/java/com/yws/com/yws/service/StockService.java
@@ -9,4 +9,19 @@ public class StockService {
 
     @Autowired
     private StringRedisTemplate redisTemplate;
+
+    public void deduct() {
+        //1.查询库存信息
+        String stock = redisTemplate.opsForValue().get("stock");
+
+        //2.判断库存是否充足
+        if (stock != null && stock.length() > 0) {
+            Integer st = Integer.valueOf(stock);
+            if (st > 0) {
+                //3.扣减库存
+                redisTemplate.opsForValue().set("stock", String.valueOf(--st));
+            }
+        }
+    }
+
 }
